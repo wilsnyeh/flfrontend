21d3b32210462c82d4af970581d6adb23c027c57
wilson <wilsnyeh@gmail.com>
2022-06-08 16:11:43 -0700

    locations

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,29 +1,48 @@
+function createCard(title, description, pictureUrl) {
+    return `
+      <div class="card">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${description}</p>
+        </div>
+      </div>
+    `;
+  }
 window.addEventListener('DOMContentLoaded', async () => {
     const url = 'http://localhost:8000/api/conferences';
     try {
         const response = await fetch(url);
+
         if (!response.ok) { // if the fetch url abov does not work
             console.log('the response is bad dawg') // not sure if this is correct
+
         } else { // run all of this below to generate proper response
             const data = await response.json(); // to translate the json response
-            const conference = data.conferences[0]; // this is where the conference object is
-            const nameTag = document.querySelector('.card-title'); // query selector for conference title
-            nameTag.innerHTML = conference.name; // 
-            // console.log(conference)
-            const detailUrl = `http://localhost:8000${conference.href}`;
-            const detailResponse = await fetch(detailUrl);
-            if (detailResponse.ok) {
-                
-                const details = await detailResponse.json();
+            // let x = 0
+            for (let conference of data.conferences) {
+                const detailUrl = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailUrl);
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    const title = details.conference.name;
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
+                    const html = createCard(title, description, pictureUrl);
+                    const column = document.querySelector('.row');
+                    column.innerHTML += html;
+
                 // const description = details.conference.description;
-                const conferenceDetail = document.querySelector('.card-text');
-                conferenceDetail.innerHTML = details.conference.description;
-                console.log(details);
-                const conferenceImg = document.querySelector('.card-img-top');
-                conferenceImg.src = details.conference.location.picture_url;
+                // const conferenceDetail = document.querySelector('.card-text');
+                // conferenceDetail.innerHTML = details.conference.description;
+                // console.log(details);
+                // const conferenceImg = document.querySelector('.card-img-top');
+                // conferenceImg.src = details.conference.location.picture_url;
+                }
             }
         }
     } catch (e) {
+        console.error(e)
         console.log('an error has occured partner')
     }
 });
--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
@@ -99,14 +100,6 @@ def api_list_conferences(request):
 
 def api_show_conference(request, pk):
     """
-    Returns the details for the Conference model specified
-    by the pk parameter.
-
-    This should return a dictionary with the name, starts,
-    ends, description, created, updated, max_presentations,
-    max_attendees, and a dictionary for the location containing
-    its name and href.
-
     {
         "name": the conference's name,
         "starts": the date/time when the conference starts,
@@ -136,24 +129,6 @@ def api_show_conference(request, pk):
 
 @require_http_methods(["GET", "POST"])
 def api_list_locations(request):
-    """
-    Lists the location names and the link to the location.
-
-    Returns a dictionary with a single key "locations" which
-    is a list of location names and URLS. Each entry in the list
-    is a dictionary that contains the name of the location and
-    the link to the location's information.
-
-    {
-        "locations": [
-            {
-                "name": location's name,
-                "href": URL to the location,
-            },
-            ...
-        ]
-    }
-    """
     if request.method == "GET":
         locations = Location.objects.all()
         return JsonResponse(
@@ -228,3 +203,18 @@ def api_show_location(request, pk):
             encoder=LocationDetailEncoder,
             safe=False,
         )
+
+
+@require_http_methods(["GET"])
+def api_list_states(request):
+    # Get the states from the database ordered by name
+    states = State.objects.order_by('name')
+    # Create an empty list named state_list
+    state_list = []
+    # For each state in the states from the database
+    for state in states:
+        # Create a dictionary that contains the name and
+        state_list.append({state.name: state.abbreviation})
+        # abbreviation for each state
+        # Append the dictionary to the list
+    return JsonResponse({"states": state_list})
